19a4d80180579c8800577803e6785b9f423f9c9f
viet nguyen <vietnt@exoplatform.com>
2012-08-29 11:06:37 +0700

    PLFENG-852: Improve plf-git-clone.sh to not clone again, just pull to update

--- bin/plf-git-clone.sh
+++ bin/plf-git-clone.sh
@@ -33,52 +33,25 @@ RELEASE_KS_BRANCH=stable/2.1.x
 RELEASE_PLATFORM_BRANCH=stable/3.0.x
 # -----------------------------------------------------------------------------
 
-rm -rf $PRJ_DIR/*
+#rm -rf $PRJ_DIR/*
 
 echo "==============================================================================="  
 echo "Clone from GitHub all platform projects"
 echo "==============================================================================="  
 
-gitCommand kernel        clone    git@github.com:exoplatform/kernel.git
-gitCommand kernel        pull     origin
-gitCommand kernel        checkout -b $RELEASE_KERNEL_BRANCH remotes/origin/$RELEASE_KERNEL_BRANCH
-gitCommand core          clone    git@github.com:exoplatform/core.git
-gitCommand core          pull     origin
-gitCommand core          checkout -b $RELEASE_CORE_BRANCH remotes/origin/$RELEASE_CORE_BRANCH
-gitCommand ws            clone    git@github.com:exoplatform/ws.git
-gitCommand ws            pull     origin
-gitCommand ws            checkout -b $RELEASE_WS_BRANCH remotes/origin/$RELEASE_WS_BRANCH
-gitCommand jcr           clone    git@github.com:exoplatform/jcr.git
-gitCommand jcr           pull     origin
-gitCommand jcr           checkout -b $RELEASE_JCR_BRANCH remotes/origin/$RELEASE_JCR_BRANCH
-gitCommand jcr-services  clone    git@github.com:exoplatform/jcr-services.git
-gitCommand jcr-services  pull     origin
-gitCommand jcr-services  checkout -b $RELEASE_JCR_SERVICES_BRANCH remotes/origin/$RELEASE_JCR_SERVICES_BRANCH
-gitCommand exogtn        clone    git@github.com:exoplatform/exogtn.git
-gitCommand exogtn        pull     origin
-gitCommand exogtn        checkout -b $RELEASE_EXOGTN_BRANCH remotes/origin/$RELEASE_EXOGTN_BRANCH
+projects=( 'kernel' 'core' 'ws' 'jcr' 'jcr-services' 'exogtn' 'gwtframework' 'ide' 'commons' 'ecms' 'social' 'cs' 'ks' 'platform' )
+versions=( $RELEASE_KERNEL_BRANCH $RELEASE_CORE_BRANCH $RELEASE_WS_BRANCH $RELEASE_JCR_BRANCH $RELEASE_JCR_SERVICES_BRANCH $RELEASE_EXOGTN_BRANCH $RELEASE_GWTFRAMEWORK_BRANCH $RELEASE_IDE_BRANCH $RELEASE_COMMONS_BRANCH $RELEASE_ECMS_BRANCH $RELEASE_SOCIAL_BRANCH $RELEASE_CS_BRANCH $RELEASE_KS_BRANCH $RELEASE_PLATFORM_BRANCH )
+length=${#projects[@]}
+for (( i=0;i<$length;i++)); do
+  if [ ! -d $PRJ_DIR/${projects[${i}]} ]; then
+    gitCommand ${projects[${i}]} clone git@github.com:exoplatform/${projects[${i}]}.git
+    gitCommand ${projects[${i}]} checkout -b ${versions[${i}]} remotes/origin/${versions[${i}]}
+  else
+    gitCommand ${projects[${i}]} pull origin
+    gitCommand ${projects[${i}]} checkout master
+    gitCommand ${projects[${i}]} branch -D ${versions[${i}]}
+    gitCommand ${projects[${i}]} checkout -b ${versions[${i}]} remotes/origin/${versions[${i}]}
+  fi
+done
+
 #svnCommand xcmis-1.1.x         checkout --force $SVN_GOOGLE_CREDENTIALS https://xcmis.googlecode.com/svn/branches/1.1.x/
-gitCommand gwtframework  clone    git@github.com:exoplatform/gwtframework.git
-gitCommand gwtframework  pull     origin
-gitCommand gwtframework  checkout -b $RELEASE_GWTFRAMEWORK_BRANCH remotes/origin/$RELEASE_GWTFRAMEWORK_BRANCH
-gitCommand ide           clone    git@github.com:exoplatform/ide.git
-gitCommand ide           pull     origin
-gitCommand ide           checkout -b $RELEASE_IDE_BRANCH remotes/origin/$RELEASE_IDE_BRANCH
-gitCommand commons       clone    git@github.com:exoplatform/commons.git
-gitCommand commons       pull     origin
-gitCommand commons       checkout -b $RELEASE_COMMONS_BRANCH remotes/origin/$RELEASE_COMMONS_BRANCH
-gitCommand ecms          clone    git@github.com:exoplatform/ecms.git
-gitCommand ecms          pull     origin
-gitCommand ecms          checkout -b $RELEASE_ECMS_BRANCH remotes/origin/$RELEASE_ECMS_BRANCH
-gitCommand social        clone    git@github.com:exoplatform/social.git
-gitCommand social        pull     origin
-gitCommand social        checkout -b $RELEASE_SOCIAL_BRANCH remotes/origin/$RELEASE_SOCIAL_BRANCH
-gitCommand cs            clone    git@github.com:exoplatform/cs.git
-gitCommand cs            pull     origin
-gitCommand cs            checkout -b $RELEASE_CS_BRANCH remotes/origin/$RELEASE_CS_BRANCH
-gitCommand ks            clone    git@github.com:exoplatform/ks.git
-gitCommand ks            pull     origin
-gitCommand ks            checkout -b $RELEASE_KS_BRANCH remotes/origin/$RELEASE_KS_BRANCH
-gitCommand platform      clone    git@github.com:exoplatform/platform.git
-gitCommand platform      pull    origin
-gitCommand platform      checkout -b $RELEASE_PLATFORM_BRANCH remotes/origin/$RELEASE_PLATFORM_BRANCH

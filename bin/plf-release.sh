5bed2db5f767264aaa7808e2f5cbf9dcce640ac7
trangvh <trangvh@exoplatform.com>
2015-10-22 11:02:36 +0200

    Revert "skipTests for gatein"
    This reverts commit a2db811af4bf3f901f94e489d79c7b05df06590c.

--- bin/plf-release.sh
+++ bin/plf-release.sh
@@ -107,7 +107,7 @@ function beforeRelease {
 }
 
 function prepareRelease {
-  mvnCommand $1 release:prepare -Dtag=$THIS_RELEASE_VERSION -DreleaseVersion=$THIS_RELEASE_VERSION -DdevelopmentVersion=$THIS_NEXT_SNAPSHOT_VERSION -DscmCommentPrefix="[maven-release-plugin] [$THIS_RELEASE_JIRA_ID]" $THIS_RELEASE_ADDITIONAL_OPTS -Darguments="-DskipTests"
+  mvnCommand $1 release:prepare -Dtag=$THIS_RELEASE_VERSION -DreleaseVersion=$THIS_RELEASE_VERSION -DdevelopmentVersion=$THIS_NEXT_SNAPSHOT_VERSION -DscmCommentPrefix="[maven-release-plugin] [$THIS_RELEASE_JIRA_ID]" $THIS_RELEASE_ADDITIONAL_OPTS
 }
 
 function rollbackRelease {
@@ -117,7 +117,7 @@ function rollbackRelease {
 }
 
 function performRelease {
-  mvnCommand $1 release:perform -Darguments="-DskipTests"
+  mvnCommand $1 release:perform
 }
 
 function createReleaseBranch {
